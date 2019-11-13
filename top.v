3e313c101da958231f094183b0b51d1231daac66
dylan maxfield <dylmax12@gmail.com>
2019-11-12 19:42:50 -0700

    updated. reset. etc.

--- top.v
+++ top.v
@@ -14,7 +14,9 @@ wire locked, clk;
 
 assign USBPU = 0;
 
-SB_PLL40_CORE #(
+reg [1:0] move_direction;
+
+ SB_PLL40_CORE #(
                 .FEEDBACK_PATH("SIMPLE"),
                 .DIVR(4'b0000),         // DIVR =  0
                 .DIVF(7'b0110001),      // DIVF = 49
@@ -46,90 +48,100 @@ reg [3:0] tailPosition;
   //bodyMap[3][2] = 1;
 integer clk_counter;
 
+  always @(btn1 or btn2 or btn3 or btn4 or reset) begin
+    if (~btn1) begin //left
+      if (move_direction != 2) begin
+        move_direction = 0;
+      end
+    end
+    else if (~btn2) begin // up
+        if (move_direction != 3) begin
+          move_direction = 1;
+        end
+      end
+    else if (~btn3) begin //right
+        if (move_direction != 0) begin
+          move_direction = 2;
+        end
+      end
+    else if (~btn4) begin //down
+        if (move_direction != 1) begin
+          move_direction = 3;
+        end
+      end
+end
 always @(posedge clk) begin
 clk_counter <= clk_counter + 1;
 if (~reset) begin
-  headPosition = 1;
-  tailPosition = 0;
+  bodyPositionX[0] <= 2;
+  bodyPositionY[0] <= 2;
+  bodyPositionX[1] <= 3;
+  bodyPositionX[1] <= 2;
+  bodyPositionX[2] <= 4;
+  bodyPositionY[2] <= 2;
+  bodyPositionX[3] <= 5;
+  bodyPositionY[3] <= 2;
+
+  bodyMap[0] <= 0;
+  bodyMap[1] <= 0;
+  bodyMap[2] <= 0;
+  bodyMap[3] <= 0;
+  bodyMap[4] <= 0;
+  bodyMap[5] <= 0;
+  bodyMap[6] <= 0;
+  bodyMap[7] <= 0;
+  bodyMap[8] <= 0;
+  bodyMap[9] <= 0;
+  bodyMap[10] <= 0;
+  bodyMap[11] <= 0;
+  bodyMap[12] <= 0;
+  bodyMap[13] <= 0;
+
+  bodyMap[2][3] <= 1;
+  bodyMap[2][4] <= 1;
+  bodyMap[2][5] <= 1;
+
+  headPosition <= 3;
+  tailPosition <= 0;
 end
 else if (clk_counter == 8000000) begin
  clk_counter <= 0;
-	if (~btn1) begin //right
-    bodyPositionX[headPosition + 1] <= bodyPositionX[headPosition] + 1;
+	if (move_direction == 0) begin //Left
+    bodyPositionX[headPosition + 1] <= bodyPositionX[headPosition] - 1;
     bodyPositionY[headPosition + 1] <= bodyPositionY[headPosition];
     tailPosition <= tailPosition + 1;
     headPosition <= headPosition + 1;
       end
-  else if (~btn2) begin //up
+  else if (move_direction == 1) begin //up
     bodyPositionX[headPosition + 1] <= bodyPositionX[headPosition];// - 1;
     bodyPositionY[headPosition + 1] <= bodyPositionY[headPosition] - 1;
     tailPosition <= tailPosition + 1;
     headPosition <= headPosition + 1;
     end
-  else if (~btn3) begin //down
+  else if (move_direction == 2) begin //right
+  bodyPositionX[headPosition + 1] <= bodyPositionX[headPosition] + 1;
+  bodyPositionY[headPosition + 1] <= bodyPositionY[headPosition];
+  tailPosition <= tailPosition + 1;
+  headPosition <= headPosition + 1;
+  end
+  else if (move_direction == 3) begin //down
   bodyPositionX[headPosition + 1] <= bodyPositionX[headPosition];
   bodyPositionY[headPosition + 1] <= bodyPositionY[headPosition] + 1;
   tailPosition <= tailPosition + 1;
   headPosition <= headPosition + 1;
   end
-  else if (~btn4) begin //right
-  bodyPositionX[headPosition + 1] <= bodyPositionX[headPosition] - 1;
-  bodyPositionY[headPosition + 1] <= bodyPositionY[headPosition];
-  tailPosition <= tailPosition + 1;
-  headPosition <= headPosition + 1;
-  end
     bodyMap[bodyPositionY[headPosition]][bodyPositionX[headPosition]] <= 1;
-  end
     bodyMap[bodyPositionY[tailPosition]][bodyPositionX[tailPosition]] <= 0;
+  end
 end
-/////////////////////////////////////////////////////////////////
-reg [9:0] ballX;
-reg [8:0] ballY;
-reg ball_inX, ball_inY;
-
-always @(posedge clk)
-if(ball_inX==0) ball_inX <= (CounterX==ballX) & ball_inY; else ball_inX <= !(CounterX==ballX+16);
-
-always @(posedge clk)
-if(ball_inY==0) ball_inY <= (CounterY==ballY); else ball_inY <= !(CounterY==ballY+16);
-
-wire ball = ball_inX & ball_inY;
 
 /////////////////////////////////////////////////////////////////
+
 wire border = (CounterX[9:3]==0) || (CounterX[9:3]==79) || (CounterY[8:3]==0) || (CounterY[8:3]==59);
 wire paddle = bodyMap[CounterY[8:5]][CounterX[8:5]];//(CounterX>=PaddlePosition+8) && (CounterX<=PaddlePosition+24) && (CounterY[8:4]==27);
-//wire BouncingObject = border | paddle; // active if the border or paddle is redrawing itself
-
-// reg ResetCollision;
-// always @(posedge clk) ResetCollision <= (CounterY==500) & (CounterX==0);  // active only once for every video frame
-//
-// reg CollisionX1, CollisionX2, CollisionY1, CollisionY2;
-// always @(posedge clk) if(ResetCollision) CollisionX1<=0; else if(BouncingObject & (CounterX==ballX   ) & (CounterY==ballY+ 8)) CollisionX1<=1;
-// always @(posedge clk) if(ResetCollision) CollisionX2<=0; else if(BouncingObject & (CounterX==ballX+16) & (CounterY==ballY+ 8)) CollisionX2<=1;
-// always @(posedge clk) if(ResetCollision) CollisionY1<=0; else if(BouncingObject & (CounterX==ballX+ 8) & (CounterY==ballY   )) CollisionY1<=1;
-// always @(posedge clk) if(ResetCollision) CollisionY2<=0; else if(BouncingObject & (CounterX==ballX+ 8) & (CounterY==ballY+16)) CollisionY2<=1;
 
 /////////////////////////////////////////////////////////////////
-// wire UpdateBallPosition = ResetCollision;  // update the ball position at the same time that we reset the collision detectors
-//
-// reg ball_dirX, ball_dirY;
-// always @(posedge clk)
-// if(UpdateBallPosition)
-// begin
-// 	if(~(CollisionX1 & CollisionX2))        // if collision on both X-sides, don't move in the X direction
-// 	begin
-// 		ballX <= ballX + (ball_dirX ? -1 : 1);
-// 		if(CollisionX2) ball_dirX <= 1; else if(CollisionX1) ball_dirX <= 0;
-// 	end
-//
-// 	if(~(CollisionY1 & CollisionY2))        // if collision on both Y-sides, don't move in the Y direction
-// 	begin
-// 		ballY <= ballY + (ball_dirY ? -1 : 1);
-// 		if(CollisionY2) ball_dirY <= 1; else if(CollisionY1) ball_dirY <= 0;
-// 	end
-// end
 
-/////////////////////////////////////////////////////////////////
 wire R = border;
 wire G = paddle;
 wire B = border;
